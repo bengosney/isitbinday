c91a8feca8b12f7d7395dabb616d4e4782d37d83
Ben Gosney <bengosney@googlemail.com>
2020-08-28 08:50:35 +0100

    Add JWT

--- isitbinday/urls.py
+++ isitbinday/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
+
 from tasks.views import TaskViewSet, SprintViewSet
 
 router = routers.DefaultRouter()
