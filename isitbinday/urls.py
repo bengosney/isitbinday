16502f6ed5e9f726fec32b41e1aeeb95896aeda2
Ben Gosney <bengosney@googlemail.com>
2020-09-03 08:29:09 +0100

    Added refresh view

--- isitbinday/urls.py
+++ isitbinday/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from tasks.views import TaskViewSet, SprintViewSet
 
@@ -14,5 +14,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api-token-auth/', obtain_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token),
     path('admin/', admin.site.urls),
 ]
