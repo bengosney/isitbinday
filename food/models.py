190239f3954594cc6c745167a1e33a852d186a5c
Ben Gosney <bengosney@googlemail.com>
2020-11-20 09:06:33 +0000

    Better fleshed out models

--- food/models.py
+++ food/models.py
@@ -1,14 +1,14 @@
-from pprint import pprint
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext as _
 
 
-class category(models.Model):
+class UnitOfMeasure(models.Model):
 
     # Fields
-    name = models.CharField(max_length=50)
-    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    name = models.CharField(max_length=30)
     created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
 
     class Meta:
         pass
@@ -17,22 +17,44 @@ class category(models.Model):
         return str(self.name)
 
     def get_absolute_url(self):
-        return reverse("food_category_detail", args=(self.pk,))
+        return reverse("food_UnitOfMeasure_detail", args=(self.pk,))
 
     def get_update_url(self):
-        return reverse("food_category_update", args=(self.pk,))
+        return reverse("food_UnitOfMeasure_update", args=(self.pk,))
 
 
-class product(models.Model):
-    # Fields
-    name = models.CharField(max_length=50)
-    code = models.CharField(max_length=30)
-    last_updated = models.DateTimeField(auto_now=True, editable=False)
-    created = models.DateTimeField(auto_now_add=True, editable=False)
+class Stock(models.Model):
 
     # Relationships
-    categories = models.ManyToManyField("food.category")
-    brand = models.ForeignKey("food.brand", on_delete=models.CASCADE)
+    location = models.ForeignKey("food.Location", on_delete=models.CASCADE)
+    unit_of_measure = models.ForeignKey("food.UnitOfMeasure", on_delete=models.CASCADE,null=True,blank=True)
+    product = models.ForeignKey("food.Product", on_delete=models.CASCADE)
+
+    # Fields
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    added = models.DateTimeField(auto_now_add=True)
+    quantity = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return str(self.pk)
+
+    def get_absolute_url(self):
+        return reverse("food_Stock_detail", args=(self.pk,))
+
+    def get_update_url(self):
+        return reverse("food_Stock_update", args=(self.pk,))
+
+
+class Category(models.Model):
+
+    # Fields
+    name = models.CharField(max_length=50)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
 
     class Meta:
         pass
@@ -41,28 +63,50 @@ class product(models.Model):
         return str(self.name)
 
     def get_absolute_url(self):
-        return reverse("food_product_detail", args=(self.pk,))
+        return reverse("food_Category_detail", args=(self.pk,))
 
     def get_update_url(self):
-        return reverse("food_product_update", args=(self.pk,))
+        return reverse("food_Category_update", args=(self.pk,))
+
+
+class Product(models.Model):
+
+    # Relationships
+    categories = models.ManyToManyField("food.Category")
+    brand = models.ForeignKey("food.Brand", on_delete=models.CASCADE)
+
+    # Fields
+    name = models.CharField(max_length=50)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    code = models.CharField(max_length=30)
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_absolute_url(self):
+        return reverse("food_Product_detail", args=(self.pk,))
+
+    def get_update_url(self):
+        return reverse("food_Product_update", args=(self.pk,))
 
     @classmethod
     def get_or_create(cls, code, name, brandName, categories):
-        brandObject = brand.objects.get_or_create(name=brandName.split(',')[0])[0]
-        pprint(brandObject)
-        categoryObjects = [category.objects.get_or_create(name=name)[0] for name in categories.split(',')]
-
+        brandObject = Brand.objects.get_or_create(name=brandName.split(',')[0])[0]
+        categoryObjects = [Category.objects.get_or_create(name=name)[0] for name in categories.split(',')]
+ 
         prod = cls.objects.get_or_create(code=code, defaults={
             'name': name,
             'brand': brandObject,
         })[0]
-
         prod.categories.set(categoryObjects)
-
         return prod
 
 
-class brand(models.Model):
+class Brand(models.Model):
 
     # Fields
     name = models.CharField(max_length=50)
@@ -76,7 +120,38 @@ class brand(models.Model):
         return str(self.name)
 
     def get_absolute_url(self):
-        return reverse("food_brand_detail", args=(self.pk,))
+        return reverse("food_Brand_detail", args=(self.pk,))
 
     def get_update_url(self):
-        return reverse("food_brand_update", args=(self.pk,))
+        return reverse("food_Brand_update", args=(self.pk,))
+
+
+class Location(models.Model):
+
+    TYPE_ROOM = 'Room Temperature'
+    TYPE_FRIDGE = 'Fridge'
+    TYPE_FREEZER = 'Freezer'
+
+    TYPES = [
+        (TYPE_ROOM, _(TYPE_ROOM)),
+        (TYPE_FRIDGE, _(TYPE_FRIDGE)),
+        (TYPE_FREEZER, _(TYPE_FREEZER)),
+    ]
+
+    # Fields
+    type = models.CharField(max_length=30, choices=TYPES)
+    name = models.CharField(max_length=30)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_absolute_url(self):
+        return reverse("food_Location_detail", args=(self.pk,))
+
+    def get_update_url(self):
+        return reverse("food_Location_update", args=(self.pk,))
--- food/urls.py
+++ food/urls.py
@@ -6,23 +6,14 @@ from . import views
 
 
 router = routers.DefaultRouter()
-router.register("category", api.categoryViewSet)
-router.register("product", api.productViewSet)
-router.register("brand", api.brandViewSet)
+router.register("UnitOfMeasure", api.UnitOfMeasureViewSet)
+router.register("Stock", api.StockViewSet)
+router.register("Category", api.CategoryViewSet)
+router.register("Product", api.ProductViewSet)
+router.register("Brand", api.BrandViewSet)
+router.register("Location", api.LocationViewSet)
 
 urlpatterns = (
     path('', views.test),
     path("api/v1/", include(router.urls)),
-    path("category/", views.categoryListView.as_view(), name="food_category_list"),
-    path("category/create/", views.categoryCreateView.as_view(), name="food_category_create"),
-    path("category/detail/<int:pk>/", views.categoryDetailView.as_view(), name="food_category_detail"),
-    path("category/update/<int:pk>/", views.categoryUpdateView.as_view(), name="food_category_update"),
-    path("product/", views.productListView.as_view(), name="food_product_list"),
-    path("product/create/", views.productCreateView.as_view(), name="food_product_create"),
-    path("product/detail/<int:pk>/", views.productDetailView.as_view(), name="food_product_detail"),
-    path("product/update/<int:pk>/", views.productUpdateView.as_view(), name="food_product_update"),
-    path("brand/", views.brandListView.as_view(), name="food_brand_list"),
-    path("brand/create/", views.brandCreateView.as_view(), name="food_brand_create"),
-    path("brand/detail/<int:pk>/", views.brandDetailView.as_view(), name="food_brand_detail"),
-    path("brand/update/<int:pk>/", views.brandUpdateView.as_view(), name="food_brand_update"),
 )
