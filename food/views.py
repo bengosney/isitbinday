372dc24e238634f3b30b891c83086d10c34c0098
Ben Gosney <bengosney@googlemail.com>
2020-11-26 20:54:07 +0000

    Bring layout inline with tasks

--- food/views.py
+++ food/views.py
@@ -1,131 +1,85 @@
 # Django
-from django.views import generic
+
+# Third Party
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+# First Party
+from food.models import Location, Product
+from food.serializers import LookupSerializer, StockSerializer
 
 # Locals
-from . import forms, models
+from . import models, serializers
 
 
-class UnitOfMeasureListView(generic.ListView):
-    model = models.UnitOfMeasure
-    form_class = forms.UnitOfMeasureForm
+class UnitOfMeasureViewSet(viewsets.ModelViewSet):
+    """ViewSet for the UnitOfMeasure class"""
+
+    queryset = models.UnitOfMeasure.objects.all()
+    serializer_class = serializers.UnitOfMeasureSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
-class UnitOfMeasureCreateView(generic.CreateView):
-    model = models.UnitOfMeasure
-    form_class = forms.UnitOfMeasureForm
+class StockViewSet(viewsets.ModelViewSet):
+    """ViewSet for the Stock class"""
+
+    queryset = models.Stock.objects.all()
+    serializer_class = serializers.StockSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
-class UnitOfMeasureDetailView(generic.DetailView):
-    model = models.UnitOfMeasure
-    form_class = forms.UnitOfMeasureForm
+class CategoryViewSet(viewsets.ModelViewSet):
+    """ViewSet for the Category class"""
+
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
-class UnitOfMeasureUpdateView(generic.UpdateView):
-    model = models.UnitOfMeasure
-    form_class = forms.UnitOfMeasureForm
-    pk_url_kwarg = "pk"
+class ProductViewSet(viewsets.ModelViewSet):
+    """ViewSet for the Product class"""
+
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    lookup_field = 'code'
+
+    @action(detail=True)
+    def lookup(self, request, code=None):
+        try:
+            product = Product.get_or_lookup(code)
+        except Exception as e:
+            return Response(f'{type(e).__name__}: {e}', status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = LookupSerializer(product)
+        return Response(serializer.data)
+
+    @action(detail=True)
+    def transfer_in(self, request, code: str = None):
+        quantity = request.query_params.get('quantity', 1)
+        expires = request.query_params.get('expires', None)
+        location = request.query_params.get('location', Location.get_default())
+
+        product = Product.get_or_lookup(code)
+        stock = product.transfer_in(quantity, expires, location)
+        serializer = StockSerializer(stock)
+
+        return Response(serializer.data)
 
 
-class StockListView(generic.ListView):
-    model = models.Stock
-    form_class = forms.StockForm
+class BrandViewSet(viewsets.ModelViewSet):
+    """ViewSet for the Brand class"""
+
+    queryset = models.Brand.objects.all()
+    serializer_class = serializers.BrandSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
-class StockCreateView(generic.CreateView):
-    model = models.Stock
-    form_class = forms.StockForm
+class LocationViewSet(viewsets.ModelViewSet):
+    """ViewSet for the Location class"""
 
-
-class StockDetailView(generic.DetailView):
-    model = models.Stock
-    form_class = forms.StockForm
-
-
-class StockUpdateView(generic.UpdateView):
-    model = models.Stock
-    form_class = forms.StockForm
-    pk_url_kwarg = "pk"
-
-
-class CategoryListView(generic.ListView):
-    model = models.Category
-    form_class = forms.CategoryForm
-
-
-class CategoryCreateView(generic.CreateView):
-    model = models.Category
-    form_class = forms.CategoryForm
-
-
-class CategoryDetailView(generic.DetailView):
-    model = models.Category
-    form_class = forms.CategoryForm
-
-
-class CategoryUpdateView(generic.UpdateView):
-    model = models.Category
-    form_class = forms.CategoryForm
-    pk_url_kwarg = "pk"
-
-
-class ProductListView(generic.ListView):
-    model = models.Product
-    form_class = forms.ProductForm
-
-
-class ProductCreateView(generic.CreateView):
-    model = models.Product
-    form_class = forms.ProductForm
-
-
-class ProductDetailView(generic.DetailView):
-    model = models.Product
-    form_class = forms.ProductForm
-
-
-class ProductUpdateView(generic.UpdateView):
-    model = models.Product
-    form_class = forms.ProductForm
-    pk_url_kwarg = "pk"
-
-
-class BrandListView(generic.ListView):
-    model = models.Brand
-    form_class = forms.BrandForm
-
-
-class BrandCreateView(generic.CreateView):
-    model = models.Brand
-    form_class = forms.BrandForm
-
-
-class BrandDetailView(generic.DetailView):
-    model = models.Brand
-    form_class = forms.BrandForm
-
-
-class BrandUpdateView(generic.UpdateView):
-    model = models.Brand
-    form_class = forms.BrandForm
-    pk_url_kwarg = "pk"
-
-
-class LocationListView(generic.ListView):
-    model = models.Location
-    form_class = forms.LocationForm
-
-
-class LocationCreateView(generic.CreateView):
-    model = models.Location
-    form_class = forms.LocationForm
-
-
-class LocationDetailView(generic.DetailView):
-    model = models.Location
-    form_class = forms.LocationForm
-
-
-class LocationUpdateView(generic.UpdateView):
-    model = models.Location
-    form_class = forms.LocationForm
-    pk_url_kwarg = "pk"
+    queryset = models.Location.objects.all()
+    serializer_class = serializers.LocationSerializer
+    permission_classes = [permissions.IsAuthenticated]
