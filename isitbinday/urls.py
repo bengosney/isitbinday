f1ce1e180b5980f207026b9b83eb66090b464fff
Ben Gosney <bengosney@googlemail.com>
2020-11-26 08:52:58 +0000

    Make the food URLs consistent with the task URLs

--- isitbinday/urls.py
+++ isitbinday/urls.py
@@ -6,14 +6,15 @@ from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 # First Party
-from tasks.urls import router
+from tasks.urls import router as taskRouter
+from food.urls import router as foodRouter
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/tasks/', include(router.urls)),
+    path('api/tasks/', include(taskRouter.urls)),
+    path('api/food/', include(foodRouter.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/food/', include('food.urls')),
     path('admin/', admin.site.urls),
 ]
