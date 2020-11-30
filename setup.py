009d8e5f56a0e60ec793696f0867832d2e75a7dd
Ben Gosney <bengosney@googlemail.com>
2020-11-30 08:36:53 +0000

    Add deps to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,23 @@ from setuptools import setup, find_packages
 
 setup(name='isitbinday',
       version='1.0',
-      packages=find_packages()
-      scripts=['manage.py'])
+      packages=find_packages(),
+      scripts=['manage.py'],
+      install_requires=[
+          'django',
+          'django-admin-sortable2',
+          'django-appconf',
+          'django-cors-headers',
+          'django-fsm',
+          'django-fsm-admin',
+          'django-fsm-log',
+          'django-heroku',
+          'django-oso',
+          'djangorestframework',
+          'djangorestframework-simplejwt', 
+          'googletrans',
+          'rollbar',
+          'openfoodfacts',
+          'recurrent',
+          ],
+      )
