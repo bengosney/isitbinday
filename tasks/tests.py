270133c431eb659fbd3c7818332a6c4319270824
Ben Gosney <bengosney@googlemail.com>
2020-12-01 14:25:27 +0000

    Start testing the api

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,16 +1,56 @@
 # Standard Library
 import datetime
+import random
+import string
+from abc import ABC
 
 # Django
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
+
+# Third Party
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Locals
 from .models import Task
 
 
-class TaskTestCase(TestCase):
+def getInsecurePassword(length):
+    return "".join(random.choice(string.ascii_lowercase) for i in range(length))  # nosec
+
+
+class APITestCaseWithUser(ABC, APITestCase):
+    def setUp(self):
+        self.password = getInsecurePassword(12)
+        self.user = User.objects.create_user(username="jacob", email="jacob@example.com", password=self.password)
+
+
+class TaskAuthTestCase(APITestCaseWithUser):
+    def test_requires_auth(self):
+        url = reverse("task-list")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class TaskViewsTestCase(APITestCaseWithUser):
+    def setUp(self):
+        super().setUp()
+        self.client.login(username=self.user.username, password=self.password)
+
+    def test_create(self):
+        url = reverse("task-list")
+        data = {
+            "title": "test task",
+        }
+
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class TaskModelTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="jacob", email="jacob@example.com")
 
