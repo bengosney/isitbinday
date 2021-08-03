e9b923490ab519264d6338467915022a09b207ee
Ben Gosney <bengosney@googlemail.com>
2021-08-03 08:58:22 +0100

    Linting

--- tasks/permissions.py
+++ tasks/permissions.py
@@ -8,9 +8,7 @@ from rest_framework import permissions
 
 
 class IsOwner(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object
-    """
+    """Custom permission to only allow owners of an object."""
 
     def has_object_permission(self, request, view, obj):
         print(f"has_object_permission: {view.action}")
