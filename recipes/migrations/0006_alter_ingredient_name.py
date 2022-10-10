7da51b6d26a1c36402e14eb653d91cb44979b6a7
Ben Gosney <bengosney@googlemail.com>
2022-10-10 08:31:57 +0100

    Make the bad migrations inert

--- recipes/migrations/0006_alter_ingredient_name.py
+++ recipes/migrations/0006_alter_ingredient_name.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name="ingredient",
             name="name",
-            field=models.CharField(max_length=30, unique=True),
+            field=models.CharField(max_length=30, unique=False),
         ),
     ]
