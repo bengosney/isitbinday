bd37eabb30ceb70d917e64a6f4671bd3eebb18be
Ben Gosney <bengosney@googlemail.com>
2024-05-13 21:21:10 +0100

    More static config

--- isitbinday/settings/base.py
+++ isitbinday/settings/base.py
@@ -120,7 +120,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STORAGES = {
     "default": {
--- isitbinday/settings/production.py
+++ isitbinday/settings/production.py
@@ -57,7 +57,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 # AWS_S3_CUSTOM_DOMAIN = "cdn.isitbinday.com"
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 with contextlib.suppress(KeyError):
     EMAIL_HOST = env["SMTP_HOST"]
