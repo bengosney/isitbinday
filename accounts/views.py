c0937f929522a601f614e8465aa80087d870df45
Ben Gosney <bengosney@googlemail.com>
2024-05-27 21:26:19 +0100

    refactor(urls): split and import account urls corectly

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 # Django
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def dashboard(request):
     return render(request, "accounts/dashboard.html")
