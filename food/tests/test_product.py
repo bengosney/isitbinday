6be6781bf5917ec2ff92b1dcc6bc0959e37cb7ad
Ben Gosney <bengosney@googlemail.com>
2024-05-31 16:09:37 +0100

    test(food): refactor tests to pytest

--- food/tests/test_product.py
+++ food/tests/test_product.py
@@ -2,35 +2,40 @@
 import random
 import string
 
+# Third Party
+import pytest
+
 # Locals
 from ..models import Product, Stock
-from .base import BaseTestCase
 
 
-class ProductTestCase(BaseTestCase):
-    def test_transfer_in(self):
-        stock = self.product.transfer_in(self.user, quantity=2)
-        self.assertIsInstance(stock, Stock)
-        self.assertEqual(stock.quantity, 2)
+@pytest.mark.django_db
+def test_transfer_in(product, user):
+    stock = product.transfer_in(user, quantity=2)
+    assert isinstance(stock, Stock)
+    assert stock.quantity == 2
 
-    def test_create_product(self):
-        args = {
-            "categories": "Stock Cubes, Stock",
-            "code": "".join(random.choice(string.digits) for _ in range(13)),
-            "name": "Beef Stock Cubes",
-            "brand": "ASDA",
-            "quantity": "12",
-            "unit_of_measure": "Each",
-        }
 
-        product = Product.get_or_create(**args)
+@pytest.mark.django_db
+def test_create_product():
+    args = {
+        "categories": "Stock Cubes, Stock",
+        "code": "".join(random.choice(string.digits) for _ in range(13)),
+        "name": "Beef Stock Cubes",
+        "brand": "ASDA",
+        "quantity": "12",
+        "unit_of_measure": "Each",
+        "is_pack": True,
+    }
 
-        self.assertIsInstance(product, Product)
-        for key, value in args.items():
-            prop = getattr(product, key)
-            try:
-                prop = ", ".join(f"{p.name}" for p in prop.all())
-            except AttributeError:
-                pass
+    product = Product.get_or_create(**args)
 
-            self.assertEqual(f"{key}: {prop}", f"{key}: {value}")
+    assert isinstance(product, Product)
+    for key, value in args.items():
+        prop = getattr(product, key)
+        try:
+            prop = ", ".join(f"{p.name}" for p in prop.all())
+        except AttributeError:
+            pass
+
+        assert f"{key}: {prop}" == f"{key}: {value}"
--- food/tests/test_product_api.py
+++ food/tests/test_product_api.py
@@ -6,57 +6,62 @@ from django.urls import reverse
 from django.utils.http import urlencode
 
 # Third Party
+import pytest
 from rest_framework import status
 
 # Locals
 from ..models import Stock
-from .base import APIBaseTestCase
 
 
-class TestProductAPI(APIBaseTestCase):
-    def setUp(self):
-        super().setUp()
-        self.login()
+@pytest.mark.django_db
+def test_transfer_in(authenticated_client, product):
+    url = reverse("food:product-transfer-in", kwargs={"code": product.code})
+    response = authenticated_client.get(url, format="json")
+    assert response.status_code == status.HTTP_200_OK
 
-    def test_transfer_in(self):
-        url = reverse("food:product-transfer-in", kwargs={"code": self.product.code})
-        response = self.client.get(url, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    stocks = product.stocks.all()
+    assert len(stocks) == 1
+    assert stocks[0].quantity == 1
+    assert stocks[0].state == Stock.STATE_IN_STOCK
 
-        stocks = self.product.stocks.all()
-        self.assertEqual(len(stocks), 1)
-        self.assertEqual(stocks[0].quantity, 1)
-        self.assertEqual(stocks[0].state, Stock.STATE_IN_STOCK)
 
-    def test_transfer_quantity_in(self, quantity=10):
-        url = reverse("food:product-transfer-in", kwargs={"code": self.product.code})
-        url_with_query = f"{url}?{urlencode({'quantity': quantity})}"
-        response = self.client.get(url_with_query, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+@pytest.mark.django_db
+def test_transfer_quantity_in(authenticated_client, product):
+    quantity = 10
+    url = reverse("food:product-transfer-in", kwargs={"code": product.code})
+    url_with_query = f"{url}?{urlencode({'quantity': quantity})}"
+    response = authenticated_client.get(url_with_query, format="json")
+    assert response.status_code == status.HTTP_200_OK
 
-        stocks = self.product.stocks.all()
-        self.assertEqual(len(stocks), 1)
-        self.assertEqual(stocks[0].quantity, quantity)
-        self.assertEqual(stocks[0].state, Stock.STATE_IN_STOCK)
+    stocks = product.stocks.all()
+    assert len(stocks) == 1
+    assert stocks[0].quantity == quantity
+    assert stocks[0].state == Stock.STATE_IN_STOCK
 
-    def test_transfer_pack_in(self, quantity=2):
-        url = reverse("food:product-transfer-in", kwargs={"code": self.pack.code})
-        url_with_query = f"{url}?{urlencode({'quantity': quantity})}"
-        response = self.client.get(url_with_query, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        stocks = self.pack.stocks.all()
-        self.assertEqual(len(stocks), 1)
-        self.assertEqual(stocks[0].quantity, quantity * self.pack.quantity)
-        self.assertEqual(stocks[0].state, Stock.STATE_IN_STOCK)
+@pytest.mark.django_db
+def test_transfer_pack_in(authenticated_client, pack):
+    quantity = 2
+    url = reverse("food:product-transfer-in", kwargs={"code": pack.code})
+    url_with_query = f"{url}?{urlencode({'quantity': quantity})}"
+    response = authenticated_client.get(url_with_query, format="json")
+    assert response.status_code == status.HTTP_200_OK
 
-    def test_consume_some(self, quantity=10):
-        stock = self.product.transfer_in(self.user, quantity)
-        url = reverse("food:stock-consume", kwargs={"pk": stock.pk})
-        to_consume = quantity // 2
-        url_with_query = f"{url}?{urlencode({'quantity': to_consume})}"
-        response = self.client.get(url_with_query, format="json")
+    stocks = pack.stocks.all()
+    assert len(stocks) == 1
+    assert stocks[0].quantity == quantity * pack.quantity
+    assert stocks[0].state == Stock.STATE_IN_STOCK
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        content = json.loads(response.content)
-        self.assertEqual(content["quantity"], quantity - to_consume)
+
+@pytest.mark.django_db
+def test_consume_some(authenticated_client, product, user):
+    quantity = 10
+    stock = product.transfer_in(user, quantity)
+    url = reverse("food:stock-consume", kwargs={"pk": stock.pk})
+    to_consume = quantity // 2
+    url_with_query = f"{url}?{urlencode({'quantity': to_consume})}"
+    response = authenticated_client.get(url_with_query, format="json")
+
+    assert response.status_code == status.HTTP_200_OK
+    content = json.loads(response.content)
+    assert content["quantity"] == quantity - to_consume
