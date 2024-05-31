40ec01df89d61dd51b48a1268e10fe96bbb2425f
Ben Gosney <bengosney@googlemail.com>
2024-05-31 09:58:11 +0100

    test(recipes): refactor tests to pytest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,40 +1,25 @@
 # Standard Library
 import inspect
-import random
-import string
-from abc import ABC
+from datetime import timedelta
 
 # Django
 from django.contrib.auth.models import User
-from django.test import TestCase
 from django.urls import reverse
 
 # Third Party
 import pint
+import pytest
 from rest_framework import status
-from rest_framework.test import APITestCase
 
 # Locals
 from .models import Ingredient, Recipe, Unit
 from .serializers import IngredientSerializer
 
 
-def get_insecure_password(length):
-    return "".join(random.choice(string.ascii_lowercase) for _ in range(length))
-
-
-class APITestCaseWithUser(ABC, APITestCase):
-    def setUp(self):
-        self.password = get_insecure_password(12)
-        self.user = User.objects.create_user(username="jacob", email="jacob@example.com", password=self.password)
-
-
-class RecipeViewsTestCase(APITestCaseWithUser):
-    def setUp(self):
-        super().setUp()
-        self.client.login(username=self.user.username, password=self.password)
-
-    def create_recipes(self, count):
+@pytest.fixture
+def create_recipes(authenticated_client):
+    def _create_recipes(count, client=None):
+        _client = client or authenticated_client
         url = reverse("recipes:recipe-list")
 
         for i in range(count):
@@ -44,134 +29,140 @@ class RecipeViewsTestCase(APITestCaseWithUser):
                 "description": "string",
                 "link": "http://example.com",
             }
-            self.assertEqual(
-                status.HTTP_201_CREATED,
-                self.client.post(url, data, format="json").status_code,
-            )
+            _client.post(url, data, format="json").status_code
 
-    def test_create(self, data={}):
-        url = reverse("recipes:recipe-list")
-        print(url)
-        name = "Test Recipe"
-        default_data = {
-            "name": name,
-            "time": "00:00:00",
-            "description": "string",
-            "link": "http://example.com",
-        }
-
-        response = self.client.post(url, {**default_data, **data}, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Recipe.objects.count(), 1)
-        self.assertEqual(name, Recipe.objects.get(name=name).name)
-
-    def test_list(self):
-        url = reverse("recipes:recipe-list")
-        count = 5
-
-        self.create_recipes(count)
-
-        response = self.client.get(url, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Recipe.objects.count(), count)
-        self.assertEqual(Recipe.objects.count(), int(response.json()["count"]))
-
-    def test_list_only_mine(self):
-        password = get_insecure_password(12)
-        second_user = User.objects.create_user(username="keith", email="keith@example.com", password=password)
-
-        url = reverse("recipes:recipe-list")
-        count = 5
-
-        self.create_recipes(count)
-        self.client.logout()
-
-        self.client.login(username=second_user.username, password=password)
-        self.create_recipes(count)
-
-        response = self.client.get(url, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Recipe.objects.count() - count, int(response.json()["count"]))
+    return _create_recipes
 
 
-class UnitsTestCases(TestCase):
-    def test_unit_class(self):
-        unit = Unit(name="g")
-        self.assertIsInstance(unit.unit_class, pint.Quantity)
+@pytest.mark.django_db
+def test_create(authenticated_client):
+    url = reverse("recipes:recipe-list")
+    name = "Test Recipe"
+    default_data = {
+        "name": name,
+        "time": "00:00:00",
+        "description": "string",
+        "link": "http://example.com",
+    }
 
-    def test_unit_class_type(self):
-        unit = Unit(name="g")
-        self.assertEqual(unit.unit_class.units, "gram")
-
-    def test_unit_name_with_spaces(self):
-        unit = Unit(name="Imperial Pint")
-
-        self.assertIsInstance(unit.unit_class, pint.Quantity)
-        self.assertEqual(unit.unit_class.units, "imperial_pint")
-
-    def test_cooking_system(self):
-        unit = Unit(name="ml")
-        ing = Ingredient(name="water", unit=unit, quantity=300)
-
-        self.assertEqual(ing.quantity_metric_unit, "millilitres")
-
-    def test_invalid_unit(self):
-        unit_name = "qwerty"
-        unit = Unit(name=unit_name)
-        ing = Ingredient(name="stuff", unit=unit, quantity=5)
-
-        self.assertEqual(ing.quantity_metric, 5)
-        self.assertEqual(ing.quantity_metric_unit, unit_name)
+    response = authenticated_client.post(url, default_data, format="json")
+    assert response.status_code == status.HTTP_201_CREATED
+    assert Recipe.objects.count() == 1
 
 
-class IngredientTestCases(TestCase):
-    def setUp(self) -> None:
-        self.units = [
-            (Unit(name="gram"), "gram"),
-            (Unit(name="oz"), "ounce"),
-        ]
+@pytest.mark.django_db
+def test_list(create_recipes, authenticated_client):
+    url = reverse("recipes:recipe-list")
 
-        return super().setUp()
+    create_recipes(5)
 
-    def test_unit_class(self):
-        for u, _ in self.units:
-            i = Ingredient(name="flour", unit=u, quantity=300.0)
+    response = authenticated_client.get(url, format="json")
+    assert response.status_code == status.HTTP_200_OK
+    assert Recipe.objects.count() == 5
+    assert Recipe.objects.count() == int(response.json()["count"])
 
-            self.assertIsInstance(i.quantity_class, pint.Quantity)
 
-    def test_unit_names(self):
-        qty = 300.0
-        for u, name in self.units:
-            i = Ingredient(name="flour", unit=u, quantity=qty)
+@pytest.mark.django_db
+def test_list_only_mine(insecure_password, create_recipes, authenticated_client):
+    url = reverse("recipes:recipe-list")
+    count = 5
 
-            self.assertEqual(f"{i.quantity_class}", f"{qty} {name}")
-
-    def test_base_unit(self):
-        i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
-        self.assertAlmostEqual(i.quantity_base_units.magnitude, 283.495, 3)
-
-    def test_metric_quantity(self):
-        i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
-        self.assertAlmostEqual(i.quantity_metric, 283.495, 3)
-
-    def test_metric_unit(self):
-        i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
-        self.assertEqual(i.quantity_metric_unit, "gram")
-
-    def test_serializer_quantity(self):
-        i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
-        serializer = IngredientSerializer(i)
-
-        self.assertEqual(
-            f"{serializer.data['quantity_metric']}",
-            f"{i.quantity_base_units.magnitude}",
+    second_user = User.objects.create_user(username="keith", email="keith@example.com", password=insecure_password)
+    for i in range(count):
+        Recipe.objects.create(
+            name=f"Other Recipe {i}",
+            time=timedelta(minutes=i),
+            description="string",
+            link="http://example.com",
+            owner=second_user,
         )
 
-    def test_serializer_unit(self):
-        i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
-        serializer = IngredientSerializer(i)
+    create_recipes(count)
 
-        self.assertEqual(
-            f"{serializer.data['quantity_metric_unit']}",
-            f"{i.quantity_base_units.units}",
-        )
+    response = authenticated_client.get(url, format="json")
+    assert response.status_code == status.HTTP_200_OK
+    assert Recipe.objects.count() - count == int(response.json()["count"])
+
+
+def test_unit_class():
+    unit = Unit(name="g")
+    assert isinstance(unit.unit_class, pint.Quantity)
+
+
+def test_unit_class_type():
+    unit = Unit(name="g")
+    assert unit.unit_class is not None
+    assert unit.unit_class.units == "gram"
+
+
+def test_unit_name_with_spaces():
+    unit = Unit(name="Imperial Pint")
+    assert isinstance(unit.unit_class, pint.Quantity)
+    assert unit.unit_class.units == "imperial_pint"
+
+
+def test_cooking_system():
+    unit = Unit(name="ml")
+    ing = Ingredient(name="water", unit=unit, quantity=300)
+
+    assert ing.quantity_metric_unit == "millilitres"
+
+
+def test_invalid_unit():
+    unit_name = "qwerty"
+    unit = Unit(name=unit_name)
+    ing = Ingredient(name="stuff", unit=unit, quantity=5)
+
+    assert ing.quantity_metric == 5
+    assert ing.quantity_metric_unit == unit_name
+
+
+@pytest.fixture
+def units():
+    return [
+        (Unit(name="gram"), "gram"),
+        (Unit(name="oz"), "ounce"),
+    ]
+
+
+def test_ingredient_unit_class(units):
+    for u, _ in units:
+        i = Ingredient(name="flour", unit=u, quantity=300.0)
+        assert isinstance(i.quantity_class, pint.Quantity)
+
+
+def test_unit_names(units):
+    qty = 300.0
+    for u, name in units:
+        i = Ingredient(name="flour", unit=u, quantity=qty)
+
+        assert f"{i.quantity_class}" == f"{qty} {name}"
+
+
+def test_base_unit():
+    i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
+    assert i.quantity_base_units.magnitude == pytest.approx(283.495, 3)
+
+
+def test_metric_quantity():
+    i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
+    assert i.quantity_metric == pytest.approx(283.495, 3)
+
+
+def test_metric_unit():
+    i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
+    assert i.quantity_metric_unit == "gram"
+
+
+def test_serializer_quantity():
+    i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
+    serializer = IngredientSerializer(i)
+
+    assert f"{serializer.data['quantity_metric']}" == f"{i.quantity_base_units.magnitude}"
+
+
+def test_serializer_unit():
+    i = Ingredient(name="flour", unit=Unit(name="oz"), quantity=10.0)
+    serializer = IngredientSerializer(i)
+
+    assert f"{serializer.data['quantity_metric_unit']}" == f"{i.quantity_base_units.units}"
