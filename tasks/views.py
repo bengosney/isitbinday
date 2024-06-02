bc96b4804029aa54269dc4c4ac01386acf701f34
Ben Gosney <bengosney@googlemail.com>
2024-06-02 19:47:24 +0100

    feat(tasks): basic task view

--- tasks/views.py
+++ tasks/views.py
@@ -7,3 +7,14 @@ from .models import Task
 
 class TaskListView(ListView):
     model = Task
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tasks: dict[str, list[Task]] = {state: [] for state in Task.STATES if state != "archive"}
+
+        for task in context["object_list"]:
+            tasks[task.state].append(task)
+
+        context["state_tasks"] = tasks
+
+        return context
