1faac7381900c5f3757e6b4b6773dda662162653
Ben Gosney <bengosney@googlemail.com>
2024-06-08 09:59:54 +0100

    feat(tasks): drag over moves the items out of the way

--- assets/typescript/main.ts
+++ assets/typescript/main.ts
@@ -1,15 +1,19 @@
-import './external/alpine';
-import './external/htmx';
-import { dragStart, dragEnd} from './modules/dragdrop';
+import "./external/alpine";
+import "./external/htmx";
+import { dragStart, dragEnd, dragOver, dragLeave } from "./modules/dragdrop";
 
 declare global {
-    interface Window {
-        dragStart: typeof dragStart;
-        dragEnd: typeof dragEnd;
-    }
+  interface Window {
+    dragStart: typeof dragStart;
+    dragEnd: typeof dragEnd;
+    dragOver: typeof dragOver;
+    dragLeave: typeof dragLeave;
+  }
 }
 
 const Window = window;
 
 window.dragStart = dragStart;
 window.dragEnd = dragEnd;
+window.dragOver = dragOver;
+window.dragLeave = dragLeave;
--- assets/typescript/modules/dragdrop.ts
+++ assets/typescript/modules/dragdrop.ts
@@ -1,45 +1,79 @@
-const dragClass = 'dragging';
+import { takeClass } from "htmx.org";
+
+const dragClass = "dragging";
+
+const clearDragOver = () => {
+  ["move-up", "move-down"].forEach((selector) => {
+    document.querySelectorAll(`.${selector}`).forEach((element) => element.classList.remove(selector));
+  });
+};
 
 export const dragStart = (event: DragEvent) => {
-    if (event.dataTransfer !== null && event.target !== null && event.target instanceof HTMLElement !== false) {
-        const { target, dataTransfer } = event;
+  clearDragOver();
+  if (event.dataTransfer !== null && event.target instanceof HTMLElement !== false) {
+    const { target, dataTransfer } = event;
 
-        setTimeout(() => target.classList.add(dragClass), 10);
+    setTimeout(() => target.classList.add(dragClass), 10);
 
-        dataTransfer.effectAllowed = 'move';
-        dataTransfer.setData('text/plain', event.target.id);
+    dataTransfer.effectAllowed = "move";
+    dataTransfer.setData("text/plain", target.id);
 
-        const { width, height, left, top } = event.target.getBoundingClientRect();
-        target.style.height = `${height}px`;
-        const { clientX, clientY } = event;
-
-        const x = clientX - left;
-        const y = clientY - top;
-
-        const clone = target.cloneNode(true) as HTMLElement;
-
-        clone.classList.add('drag-ghost');
-        clone.style.position = 'absolute';
-        clone.style.top = `-${height * 10}px`;
-        clone.style.width = `${width}px`;
-        clone.style.height = `${height}px`;
-        document.body.appendChild(clone);
-        dataTransfer.setDragImage(clone, x, y);
+    const { width, height, left, top } = target.getBoundingClientRect();
+    target.style.height = `${height}px`;
+    const taskList = target.closest(".task-list");
+    if (taskList instanceof HTMLElement !== false) {
+      taskList.style.setProperty("--move-height", `${height}px`);
     }
-}
+    const { clientX, clientY } = event;
+
+    const x = clientX - left;
+    const y = clientY - top;
+
+    const clone = target.cloneNode(true) as HTMLElement;
+
+    clone.classList.add("drag-ghost");
+    clone.style.position = "absolute";
+    clone.style.top = `-${height * 10}px`;
+    clone.style.width = `${width}px`;
+    clone.style.height = `${height}px`;
+    document.body.appendChild(clone);
+    dataTransfer.setDragImage(clone, x, y);
+  }
+};
 
 export const dragEnd = (event: DragEvent) => {
-    document.querySelectorAll('.drag-ghost').forEach((ghost) => {
-        ghost.remove();
-    });
+  document.querySelectorAll(".drag-ghost").forEach((ghost) => ghost.remove());
 
-    if (event.target !== null && event.target instanceof HTMLElement !== false) {
-        console.log('dragEnd', event.target.classList);
-        const { target } = event;
+  if (event.target !== null && event.target instanceof HTMLElement !== false) {
+    const { target } = event;
 
-        target.classList.remove(dragClass);
-        target.addEventListener('transitionend', () => {
-            target.style.height = '';
-        }, { once: true });
+    target.classList.remove(dragClass);
+    target.addEventListener(
+      "transitionend",
+      () => {
+        target.style.height = "";
+        clearDragOver();
+      },
+      { once: true },
+    );
+  }
+};
+
+export const dragOver = (event: DragEvent) => {
+  clearDragOver();
+  if (event.target !== null && event.target instanceof HTMLElement !== false) {
+    const target = event.target.closest(".task") as HTMLElement;
+    const { height, top } = target.getBoundingClientRect();
+    const { clientY } = event;
+
+    if (clientY < top + height / 2) {
+      target.classList.add("move-down");
+    } else {
+      target.classList.add("move-up");
     }
-}
+  }
+};
+
+export const dragLeave = (event: DragEvent) => {
+  clearDragOver();
+};
