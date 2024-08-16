8117e6d2debc6eb71e8ef4ed57b52885dc140cf4
Ben Gosney <bengosney@googlemail.com>
2024-08-16 20:46:39 +0100

    feat(books): make the settings id for couch sync optional

--- books/management/commands/couchsync.py
+++ books/management/commands/couchsync.py
@@ -10,13 +10,17 @@ class Command(BaseCommand):
     help = "Sync with CouchDB"
 
     def add_arguments(self, parser):
-        parser.add_argument("id", type=int, help="ID to sync with CouchDB")
+        parser.add_argument("id", type=int, nargs="?", help="ID to sync with CouchDB")
 
     def handle(self, *args, **options):
-        id = options["id"]
-        settings = SyncSetting.objects.get(pk=id)
-        try:
-            sync_with_couchdb(settings, lambda line: self.stdout.write(self.style.SUCCESS(line)))
-            self.stdout.write(self.style.SUCCESS(f"Successfully synced with CouchDB for ID {id}"))
-        except Exception as e:
-            raise CommandError(f"Error syncing with CouchDB: {e}")
+        if id := options.get("id"):
+            settings_to_process = [SyncSetting.objects.get(pk=id)]
+        else:
+            settings_to_process = SyncSetting.objects.all()
+
+        for settings in settings_to_process:
+            try:
+                sync_with_couchdb(settings, lambda line: self.stdout.write(self.style.SUCCESS(line)))
+                self.stdout.write(self.style.SUCCESS(f"Successfully synced with CouchDB for ID {id}"))
+            except Exception as e:
+                raise CommandError(f"Error syncing with CouchDB: {e}")
