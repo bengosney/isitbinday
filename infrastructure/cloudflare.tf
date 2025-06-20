aa797a072954e12581b4261adebcc6a0825a49b5
Ben Gosney <bengosney@googlemail.com>
2025-06-20 22:29:35 +0100

    fix(cloudflare): rename 'value' to 'content' for CNAME records

--- infrastructure/cloudflare.tf
+++ infrastructure/cloudflare.tf
@@ -3,7 +3,7 @@ resource "cloudflare_record" "cname_dkim" {
 
   zone_id         = var.zoneid
   name            = "${aws_sesv2_email_identity.email.dkim_signing_attributes[0].tokens[count.index]}._domainkey.${var.domain}"
-  value           = "${aws_sesv2_email_identity.email.dkim_signing_attributes[0].tokens[count.index]}.dkim.amazonses.com"
+  content         = "${aws_sesv2_email_identity.email.dkim_signing_attributes[0].tokens[count.index]}.dkim.amazonses.com"
   type            = "CNAME"
   proxied         = false
   comment         = "DKIM ${count.index} - SES"
@@ -15,7 +15,7 @@ resource "cloudflare_record" "api" {
   name            = "api"
   proxied         = true
   type            = "CNAME"
-  value           = var.hosting_domain
+  content         = var.hosting_domain
   zone_id         = var.zoneid
   allow_overwrite = true
 }
